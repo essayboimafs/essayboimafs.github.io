922ea277e1224ee5d9f1ddc8658d45b6a4a27a28
essayboimafs <46035809+essayboimafs@users.noreply.github.com>
2019-01-25 23:16:05 -0700

    Update whiteboard.js

--- twitch/whiteboard.js
+++ twitch/whiteboard.js
@@ -1,8 +1,10 @@
 function setup(){
 createCanvas(1000,1000);
-  console.log("hi");
+  console.log("hil");
 }
 function draw(){
   fill(255,0,0);
  rect(100, 800, 800, 100);
+  line(266, 900, 266, 800);
+  line(534, 900, 534, 800);
 }
